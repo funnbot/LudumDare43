d6e9606460f6f8689ae53a89d3eebeacb037a55c
funnbot <dillonbayless1@gmail.com>
2018-12-01 12:02:39 -0800

    Rearrange folder structure

---
+++ Assets/Scripts/Slime/SlimeMovement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class SlimeMovement : MonoBehaviour
+{
+    // Essentially just a marble controller
+
+    public float MovementSpeed;
+    public float Decceleration;
+
+    private Rigidbody rb;
+    private float savedVelocity;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();   
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void SetActive(bool active)
+    {
+        if (active)
+        {
+            savedVelocity
+        }
+    }
+
+    private GetInput()
+    {
+
+    }
+}
