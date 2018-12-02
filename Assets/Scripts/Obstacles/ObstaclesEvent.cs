b9f510ce0945a2166448adb41348cea6980b87e7
LunaTicode <mak98k@gmail.com>
2018-12-02 23:59:21 +0200

    Level design

---
+++ Assets/Scripts/Obstacles/ObstaclesEvent.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstaclesEvent : MonoBehaviour
+{
+	[SerializeField] private float _timeBetweenTriggers = 5f;
+
+	private void OnCollisionEnter(Collision collision)
+	{
+		if (collision.gameObject.GetComponent("SlimeController"))
+		{
+			Controller.Health = -1;
+		}
+	}
+}
+
+
