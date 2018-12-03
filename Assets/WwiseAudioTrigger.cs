9bb62a79361225b6af7042ec93322802f07cdac2
LunaTicode <mak98k@gmail.com>
2018-12-03 02:04:56 +0200

    Add audio trigger

---
+++ Assets/WwiseAudioTrigger.cs
@@ -0,0 +1,57 @@
+﻿/* Created by Luna.Ticode */
+
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+using TMPro;
+using AK;
+
+public class WwiseAudioTrigger : MonoBehaviour
+{
+	private AkEvent _akEvent;
+
+	public void Trigger()
+	{
+		AkSoundEngine.PostEvent(this._akEvent.data.Id, this.gameObject);
+	}
+
+	private void Awake()
+	{
+		this._akEvent = this.GetComponent<AkEvent>();
+	}
+
+#if UNITY_EDITOR
+	//protected override void OnDrawGizmos()
+	//{
+	//}
+#endif
+}
+
+#if UNITY_EDITOR
+[CustomEditor(typeof(WwiseAudioTrigger))]
+[CanEditMultipleObjects]
+public class WwiseAudioTriggerEditor : Editor
+{
+#pragma warning disable 0219, 414
+	private WwiseAudioTrigger _sWwiseAudioTrigger;
+#pragma warning restore 0219, 414
+
+	private void OnEnable()
+	{
+		this._sWwiseAudioTrigger = this.target as WwiseAudioTrigger;
+	}
+
+	public override void OnInspectorGUI()
+	{
+		DrawDefaultInspector();
+	}
+}
+#endif
